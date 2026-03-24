107fd2192198bcb532fbe73b73c02ffbd304818a
Matias-Fuentes1 <matifuentes742@gmail.com>
2026-03-24 12:13:39 -0300

    Update RFM_segmentacion.sql

--- 02_analysis_PostgreSQL/RFM_segmentacion.sql
+++ 02_analysis_PostgreSQL/RFM_segmentacion.sql
@@ -11,7 +11,7 @@ WITH rfm_initial_calc AS (
             - MAX(invoice_date)::timestamp
         ) AS recency
     FROM fact_sales
-    WHERE is_cancelled = FALSE AND customer_id IS NOT NULL
+    WHERE invoice_date IS NOT NULL
     GROUP BY customer_id
 ),
 rfm_score AS (
@@ -32,19 +32,21 @@ SELECT
     m_score,
     (r_score + f_score + m_score) AS total_rfm_score,
     CASE 
-        WHEN r_score = 4 AND f_score >= 3 AND m_score >= 3 
-            THEN 'Principales Clientes'
-        WHEN r_score >= 3 AND f_score >= 3 AND m_score >= 3 
-            THEN 'Clientes Leales'
-        WHEN r_score = 4 AND f_score <= 2 
-            THEN 'Nuevos Clientes'
-        WHEN r_score <= 2 AND f_score >= 3 
-            THEN 'En Riesgo'
-        WHEN r_score <= 2 AND f_score <= 2 
-            THEN 'Clientes Perdidos'
-        ELSE 'Otros'
-    END AS customers_segment
+    WHEN r_score = 4 AND f_score >= 3 AND m_score >= 3 
+        THEN 'Campeones'
+    WHEN r_score >= 3 AND f_score >= 3 AND m_score >= 3 
+        THEN 'Leales'
+    WHEN r_score = 4 AND f_score <= 2 
+        THEN 'Nuevos'
+    WHEN r_score <= 2 AND f_score >= 3 AND m_score >= 3
+        THEN 'En riesgo'
+    WHEN r_score <= 2 AND f_score <= 2 AND m_score >= 3
+        THEN 'No los puedo perder'
+    WHEN r_score <= 2 AND f_score <= 2 AND m_score <= 2
+        THEN 'Perdidos'
+    ELSE 'Otros'
+        END AS customers_segment
 FROM rfm_score;
 --
 SELECT *
-FROM rfm_segment
+FROM rfm_segment;
